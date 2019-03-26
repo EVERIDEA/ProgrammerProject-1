7cca839f3044e8112cf71bbf6fa41a66edd29169
Bisma <batdps@gmail.com>
2019-03-26 14:38:25 +0700

    Bisma Init

---
+++ Assets/Scripts/2DAI/GroundAI003.cs
@@ -0,0 +1,164 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+
+[RequireComponent(typeof(Rigidbody2D))]
+[RequireComponent(typeof(Seeker))]
+
+public class GroundAI003 : MonoBehaviour
+{
+    // What to chase?
+    public Transform target;
+
+    // How many times each second we will update our path
+    public float updateRate = 2f;
+
+    // Caching
+    private Seeker seeker;
+    private Rigidbody2D rb;
+
+    //The calculated path
+    public Path path;
+
+    //The AI's speed per second
+    public float speed = 300f;
+    public ForceMode2D fMode;
+
+    [HideInInspector]
+    public bool pathIsEnded = false;
+    public bool isFacingLeft;
+
+    // The max distance from the AI to a waypoint for it to continue to the next waypoint
+    public float nextWaypointDistance = 3;
+
+    // The waypoint we are currently moving towards
+    private int currentWaypoint = 0;
+
+    //Ground Check
+    private bool canJump;
+
+    void Start()
+    {
+        seeker = GetComponent<Seeker>();
+        rb = GetComponent<Rigidbody2D>();
+
+        if (target == null)
+        {
+            Debug.LogError("No Player found? PANIC!");
+            return;
+        }
+
+        // Start a new path to the target position, return the result to the OnPathComplete method
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+
+        StartCoroutine(UpdatePath());
+    }
+
+    IEnumerator UpdatePath()
+    {
+        if (target == null)
+        {
+            //TODO: Insert a player search here.
+            yield return false;
+        }
+
+        // Start a new path to the target position, return the result to the OnPathComplete method
+        seeker.StartPath(transform.position, target.position, OnPathComplete);
+
+        yield return new WaitForSeconds(1f / updateRate);
+        StartCoroutine(UpdatePath());
+    }
+
+    public void OnPathComplete(Path p)
+    {
+        Debug.Log("We got a path. Did it have an error? " + p.error);
+        if (!p.error)
+        {
+            path = p;
+            currentWaypoint = 0;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (target == null)
+        {
+            //TODO: Insert a player search here.
+            return;
+        }
+
+        //TODO: Always look at player?
+
+
+        if (path == null)
+            return;
+
+
+        if (currentWaypoint >= path.vectorPath.Count)
+        {
+            if (pathIsEnded)
+                return;
+
+            Debug.Log("End of path reached.");
+            pathIsEnded = true;
+            return;
+        }
+
+        pathIsEnded = false;
+
+        //Direction to the next waypoint
+        Vector3 dir = (path.vectorPath[currentWaypoint] - transform.position).normalized;
+        dir *= speed * Time.fixedDeltaTime;
+        if (dir.x > 0)
+        {
+            isFacingLeft = false;
+        }
+        if (dir.x < 0)
+        {
+            isFacingLeft = true;
+        }
+
+        if (canJump)
+        {
+            //Move the AI
+            rb.AddForce(dir, fMode);
+            rb.AddForce(Vector2.up *speed/2);
+        }
+
+        float dist = Vector3.Distance(transform.position, path.vectorPath[currentWaypoint]);
+        if (dist < nextWaypointDistance)
+        {
+            currentWaypoint++;
+            return;
+        }
+
+        if (isFacingLeft)
+        {
+            Debug.Log("Facing Left");
+            transform.localScale = new Vector3(1f, 1f, 0);
+        }
+
+        if (!isFacingLeft)
+        {
+            Debug.Log("Facing Right");
+            transform.localScale = new Vector3(-1f, 1f, 0);
+        }
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.collider.tag == "Ground")
+        {
+            canJump = true;
+        }
+    }
+
+    void OnCollisionExit2D(Collision2D other)
+    {
+        if (other.collider.tag == "Ground")
+        {
+            canJump = false;
+        }
+    }
+}
--- Assets/Scripts/Structures/Organisms/Enemies/Malee/EnemyMaleeControll.cs
+++ Assets/Scripts/Structures/Organisms/Enemies/Malee/EnemyMaleeControll.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyMaleeControll : MonoBehaviour {
+public class EnemyMaleeControll : MonoBehaviour
+{
     
 }
