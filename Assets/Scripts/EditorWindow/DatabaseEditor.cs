f41c4b7273a786d8423a86aab05a3ba400b96a13
firmansyahrwibowo <firmansyahrwibowo@gmail.com>
2019-04-05 14:51:19 +0700

    Editor

---
+++ Assets/Scripts/EditorWindow/DatabaseEditor.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace Everidea.Editor {
+
+    public class DatabaseEditor : EditorWindow
+    {
+
+        [MenuItem("Everidea/Database")]
+        public static void ShowWindow()
+        {
+            GetWindow<DatabaseEditor>("Database");
+        }
+
+    }
+}
